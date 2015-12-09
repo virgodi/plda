4cf85cd3d5ae8465fae9d2ecaf55237e562c8b2c
Virgile Audi <vaudi@g.harvard.edu>
2015-12-09 14:17:01 -0500

    Reorganisation of OVI

--- src/test.py
+++ src/test.py
@@ -9,26 +9,30 @@ import pyximport
 pyximport.install()
 
 import numpy as np
-sys.path.append('Variational Inference')
-import ovi
+from OnlineVariationalInference.ovi import oviLDA
+from CollapsedGibbsSampler.cgs import cgsLDA
+from LDAutil import Evaluation
+
 import time
 
 if __name__ == '__main__':
 
-    ''' LOADING THE LAS VEGAS DATA '''
+    # LOADING THE LAS VEGAS DATA 
 
-    dtm = np.load('../lv_dtm.npy').astype(int)
-    vocablv = np.load('../vocab.npy')
+    dtm = np.load('../../lv_dtm.npy').astype(int)
+    vocablv = np.load('../../vocab.npy')
 
-    ''' Initialising parameters '''
+    # Initialising parameters
 
-    ''' Batchsize '''
+    # Batchsize 
+
     S = 40 
 
     max_iter = 1000
     threshold = 0.00000001
 
-    '''Hyperparameters'''
+    # Hyperparameters
+
     tau = 512
     kappa = 0.7
     alpha = 1/40
@@ -37,32 +41,21 @@ if __name__ == '__main__':
     num_threads = 8
     
 
-    '''Constructing the model'''
-    lda = ovi.oviLDA(num_topics, num_threads)
+    # Constructing the model
+    lda = oviLDA(num_topics, num_threads)
     print ''
     print 'Fitting an Online Variational Inference model on Las Vegas restaurant reviews with 40 topics:'
-    '''Fitting the model'''
+    
+    # Fitting the model
     np.random.seed(0)
     start = time.time()
-    lda.fit(dtm,S, tau, kappa)
+    lda.fit(dtm)
     end = time.time()-start
 
     print 'Run time: %s sec with 8 threads' %(end)
-    lda.print_topic(vocablv, num_top_words=10)
-    perplexity = lda.perplexity(dtm, S, tau=512, kappa=0.7)
-    print 'Perplexity on train dataset is %s' % perplexity
+    Evaluation.print_topic(lda,vocablv, num_top_words=10)
+    print ''
+    print Evaluation.perplexity(lda,dtm)
+    print ''
+    print 'Perplexity on train dataset is %s' % lda.perplexity_train
     
-    # ''' Printing Some Topics '''
-    # print 'Some topics:'
-    # print ''
-    # topic_word = lda.topics
-    # n_top_words = 10
-    # for i, topic_dist in enumerate(topic_word):
-    #     if i in [2,4,6,10,15,23,27,28,30,37]:
-    #         topic_words = np.array(vocablv)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
-    #         print(u'Topic {}: {}'.format(i, ' '.join(topic_words)))
-    #         print ''
-    # print '' 
-    # print 'Run time: %s sec with 8 threads' %(end)
-    # print ''
-
